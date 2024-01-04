15ef851a6f4fb544a96568fca67f7d64f88eebd1
Ankush Roy <imankushroy@gmail.com>
2024-01-04 22:30:29 +0530

    shadcn setup

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <h1>
+      Home Page
+      {/* <Button>Click me</Button> */}
+    </h1>
+  );
 }
