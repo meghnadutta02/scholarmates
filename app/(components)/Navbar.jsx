6dff0a1f57cda9b24f8c8448a72cf920c3d74910
meghnadutta02 <meghnadutta02@gmail.com>
2024-01-06 22:18:45 +0530

    auth partially done

--- app/(components)/Navbar.jsx
+++ app/(components)/Navbar.jsx
@@ -1,23 +1,32 @@
 import Link from "next/link";
 import React from "react";
-
+import { getServerSession } from "next-auth";
+import { options } from "../api/auth/[...nextauth]/options";
 const navLinks = [
   { href: "/", label: "Home" },
   { href: "/posts", label: "Posts" },
-  { href: "/about", label: "About" },
-  { href: "/profile", label: "Profile" },
+  { href: "/reels", label: "Reels" },
+  { href: "/CreateUser", label: "Admin" },
 ];
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getServerSession(options);
   return (
-    <div>
-      Navbar
-      {navLinks.map((link) => (
-        <span key={link.label} className="mx-8 font-semibold">
-          <Link href={link.href}>{link.label}</Link>
-        </span>
-      ))}
-    </div>
+    <header className="flex text-white justify-between py-2 px-4 items-center left-0 right-0 top-0  bg-gray-600">
+      MySite
+      <div className="flex justify-between gap-9 font-semibold">
+        {navLinks.map((link) => (
+          <span key={link.label}>
+            <Link href={link.href}>{link.label}</Link>
+          </span>
+        ))}
+        {session ? (
+          <Link href="/api/auth/signout?callbackUrl=/">Sign Out</Link>
+        ) : (
+          <Link href="/api/auth/signin">Sign In</Link>
+        )}
+      </div>
+    </header>
   );
 };
 
--- app/(routes)/reels/page.jsx
+++ app/(routes)/reels/page.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
-const ReelsPage = () => {
-  return <div>ReelsPage</div>;
+import { getServerSession } from "next-auth";
+import { options } from "../../api/auth/[...nextauth]/options";
+import redirect from "next/navigation";
+const ReelsPage = async () => {
+  const session = await getServerSession(options);
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/reels");
+  }
+  return (
+    <div>
+      ReelsPage
+      <p>
+        {session?.user?.email}
+        <br />
+        {session?.user?.role}
+      </p>
+    </div>
+  );
 };
 
 export default ReelsPage;
