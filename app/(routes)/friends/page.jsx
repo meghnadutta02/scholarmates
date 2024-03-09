f44340b723d15e4ed0cc2cbeae96aea55e2c368f
jradityamishra <106857292+jradityamishra@users.noreply.github.com>
2024-03-09 10:40:01 +0530

    sbnbdn

--- app/(routes)/friends/page.jsx
+++ app/(routes)/friends/page.jsx
@@ -5,50 +5,50 @@ import io from "socket.io-client";
 let socket;
 
 const Page = () => {
-    const [message, setMessage] = useState("");
-    const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const [username, setUsername] = useState("");
 
-    useEffect(()=>{
-     socket=io("http://localhost:5000",{transports:['websocket']});
-      socket.on('connect', () => {
-        console.log('Successfully connected!');
-      });
-      socket.on("connect_error", (err) => {
-        // the reason of the error, for example "xhr poll error"
-        console.log(err.message);
-      
-        // some additional description, for example the status code of the initial HTTP response
-        console.log(err.description);
-      
-        // some additional context, for example the XMLHttpRequest object
-        console.log(err.context);
-      });
+  useEffect(() => {
+    socket = io("http://localhost:5000", { cors: { origin: 'http://localhost:3000' }, transports: ['websocket'] });
+    socket.on('connect', () => {
+      console.log('Successfully connected!');
+    });
+    socket.on("connect_error", (err) => {
+      // the reason of the error, for example "xhr poll error"
+      console.log(err.message);
 
-      return () => {
-        socket.disconnect();
-      };
-    },[])
+      // some additional description, for example the status code of the initial HTTP response
+      console.log(err.description);
 
-    return (
-        <div>
-            <h1>Chat app</h1>
-            <h1>Enter a username</h1>
+      // some additional context, for example the XMLHttpRequest object
+      console.log(err.context);
+    });
 
-            <input value={username} onChange={(e) => setUsername(e.target.value)} />
+    return () => {
+      socket.disconnect();
+    };
+  }, [])
 
-            <div>
-                <form >
-                    <input
-                        name="message"
-                        placeholder="enter your message"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        autoComplete={"off"}
-                    />
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Chat app</h1>
+      <h1>Enter a username</h1>
+
+      <input value={username} onChange={(e) => setUsername(e.target.value)} />
+
+      <div>
+        <form >
+          <input
+            name="message"
+            placeholder="enter your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            autoComplete={"off"}
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
