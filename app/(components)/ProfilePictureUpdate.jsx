1326307de415dd34d5e182f7e957c52a87b2a060
meghnadutta02 <meghnakha18@gmail.com>
2024-07-10 16:05:09 +0530

    Profile picture edit interface added

--- app/(components)/ProfilePictureUpdate.jsx
+++ app/(components)/ProfilePictureUpdate.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+"use client";
+import React, { useState, useRef } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-
 import { MdCameraAlt } from "react-icons/md";
 import { Button } from "@/components/ui/button";
 import { toast } from "react-toastify";
+import AvatarEditor from "react-avatar-editor";
 import Image from "next/image";
 
 const ProfilePictureUpdate = ({ user, setUser, setUser1 }) => {
   const [selectedImage, setSelectedImage] = useState(user?.profilePic);
   const [loading, setLoading] = useState(false);
   const [uploadFile, setUploadFile] = useState(null);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const editorRef = useRef(null);
+  const [scale, setScale] = useState(1);
+
   const formData = new FormData();
 
   const handleImageViewDialog = (e) => {
@@ -37,8 +42,9 @@ const ProfilePictureUpdate = ({ user, setUser, setUser1 }) => {
       return;
     }
     setLoading(true);
-    formData.append("profilePic", uploadFile);
     try {
+      const img = await getCroppedImage();
+      formData.append("profilePic", img);
       const res = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/api/users/profile/profilePic`,
         {
@@ -48,18 +54,16 @@ const ProfilePictureUpdate = ({ user, setUser, setUser1 }) => {
       );
 
       if (res.ok) {
-        const data = await res.json();
         setUser((prevUser) => ({
           ...prevUser,
-          profilePic: data.result.profilePic,
+          profilePic: URL.createObjectURL(img),
         }));
         setUser1((prevUser) => ({
           ...prevUser,
-          profilePic: data.result.profilePic,
+          profilePic: URL.createObjectURL(img),
         }));
-        setIsProfileUpdateOpen(false);
-        setLoading(false);
         toast.success("Profile updated successfully");
+        setDialogOpen(false);
       } else {
         toast.error("Profile update failed");
       }
@@ -67,36 +71,93 @@ const ProfilePictureUpdate = ({ user, setUser, setUser1 }) => {
       console.error("Error updating profile:", error);
       toast.error("Failed to update profile");
     } finally {
-      setUploadFile(null);
-      setSelectedImage(user?.profilePic);
+      resetState();
+      setLoading(false);
     }
+  };
+
+  const resetState = () => {
+    setUploadFile(null);
+    setSelectedImage(user?.profilePic);
+    setScale(1);
+    setLoading(false);
+  };
+
+  const getCroppedImage = () => {
+    return new Promise((resolve, reject) => {
+      if (editorRef.current) {
+        editorRef.current.getImageScaledToCanvas().toBlob((blob) => {
+          if (blob) {
+            resolve(blob);
+          } else {
+            reject(new Error("Failed to create cropped image"));
+          }
+        }, "image/jpeg");
+      } else {
+        reject(new Error("Editor not available"));
+      }
+    });
   };
 
   return (
     <>
-      <Dialog>
+      <Dialog
+        open={dialogOpen}
+        onOpenChange={(isOpen) => {
+          setDialogOpen(isOpen);
+          resetState();
+        }}
+      >
         <DialogTrigger>
-          {" "}
-          <MdCameraAlt className=" cursor-pointer absolute bottom-[-3px] drop-shadow-lg p-1 bg-white rounded-full right-1 h-[26px] w-[26px]  " />
+          <MdCameraAlt className="cursor-pointer absolute bottom-[-3px] drop-shadow-lg p-1 bg-white rounded-full right-1 h-[26px] w-[26px]" />
         </DialogTrigger>
-        <DialogContent className=" md:w-[22%] sm:w-[70%] w-[80%] p-2 rounded-md">
-          <div className="flex justify-center flex-col gap-3">
-            <h3 className="text-lg font-semibold ">Update Profile Picture</h3>
-            <Image
-              src={selectedImage}
-              width={200}
-              height={200}
-              alt="User"
-              className="w-full h-[300px]"
-            />
+        <DialogContent className="md:w-[22%] sm:w-[70%] w-[80%] p-2 rounded-md">
+          <div className="flex justify-center flex-col gap-3 items-center">
+            <h3 className="text-lg font-semibold text-center">
+              Update Profile Picture
+            </h3>
+            {selectedImage && !uploadFile && (
+              <Image
+                src={selectedImage}
+                alt="Profile"
+                width={250}
+                height={250}
+                className="rounded-full"
+              />
+            )}
+            {uploadFile && (
+              <div className="w-full flex flex-col items-center">
+                <AvatarEditor
+                  ref={editorRef}
+                  image={selectedImage}
+                  width={250}
+                  height={250}
+                  border={20}
+                  borderRadius={125}
+                  color={[255, 255, 255, 0.6]}
+                  scale={scale}
+                  rotate={0}
+                  className="mx-auto"
+                />
+                <input
+                  type="range"
+                  min="1"
+                  max="2"
+                  step="0.01"
+                  value={scale}
+                  onChange={(e) => setScale(parseFloat(e.target.value))}
+                  className="w-full mt-2"
+                />
+              </div>
+            )}
             <input
               type="file"
               accept="image/*"
               onChange={handleImageViewDialog}
-              className="w-full"
+              className="w-full mt-2"
             />
             <Button
-              className="w-full py-1 focus:outline-none"
+              className="w-full py-1 mt-2 focus:outline-none"
               disabled={uploadFile === null || loading}
               onClick={handleImageViewDialogClose}
             >
--- app/api/users/profile/profilePic/route.js
+++ app/api/users/profile/profilePic/route.js
@@ -7,42 +7,41 @@ import { postObject } from "@/app/config/s3";
 
 // Update user profile picture
 export async function PUT(req) {
-    try {
-        await connect();
+  try {
+    await connect();
 
-        const session = await getServerSession(options);
-        const id = session?.user?.db_id;
+    const session = await getServerSession(options);
+    const id = session?.user?.db_id;
 
-        if (!id) {
-            return NextResponse.json({ result: "User not found" }, { status: 401 });
-        }
-
-        const data = await req.formData();
-        const updatedUserData = data.get("profilePic");
-        console.log(updatedUserData)
-
-        if (!updatedUserData) {
-            return NextResponse.json({ result: "No request data" }, { status: 400 });
-        }
-
-        const byteData = await updatedUserData.arrayBuffer();
-        const buffer = Buffer.from(byteData);
-        const path = `public/${updatedUserData.name}`;
-        const coverImage = await postObject(path, buffer);
-            console.log(coverImage)
-        const user = await User.findByIdAndUpdate(
-            id,
-            { $set: { profilePic: coverImage } },
-            { new: true }
-        );
-            console.log(user);
-        if (!user) {
-            return NextResponse.json({ error: "User not found" }, { status: 404 });
-        }
-
-        return NextResponse.json({ result: user }, { status: 200 });
-    } catch (err) {
-        console.error(err);
-        return NextResponse.json({ error: err.message }, { status: 500 });
+    if (!id) {
+      return NextResponse.json({ result: "User not found" }, { status: 401 });
     }
+
+    const data = await req.formData();
+    const updatedUserData = data.get("profilePic");
+
+    if (!updatedUserData) {
+      return NextResponse.json({ result: "No request data" }, { status: 400 });
+    }
+
+    const byteData = await updatedUserData.arrayBuffer();
+    const buffer = Buffer.from(byteData);
+    const path = `public/${updatedUserData.name}`;
+    const coverImage = await postObject(path, buffer);
+
+    const user = await User.findByIdAndUpdate(
+      id,
+      { $set: { profilePic: coverImage } },
+      { new: true }
+    );
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ result: user }, { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
 }
