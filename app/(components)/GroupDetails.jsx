609463a8b8e81cb9f92d25a782c6d269c1389cc4
Ankush Roy <imankushroy@gmail.com>
2024-07-24 17:06:54 +0530

    Optimise - GroupChat, Add -message time formatting

--- app/(components)/GroupDetails.jsx
+++ app/(components)/GroupDetails.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { MdEdit } from "react-icons/md";
 import { IoMdExit } from "react-icons/io";
@@ -37,19 +37,29 @@ import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import Loading from "./Loading";
 
 const GroupDetails = ({
   groupDetails,
   setShowGroupDetails,
   setGroups,
-  setIsRoomSelected,
-  setRoomID,
+  setSelectedGroup,
   setToggleChatView,
 }) => {
-  const [description, setDescription] = useState(groupDetails.description);
-  const [isPublic, setIsPublic] = useState(groupDetails.isPublic);
-  const [group, setGroup] = useState(groupDetails);
-  const [name, setName] = useState(groupDetails.name);
+  const [description, setDescription] = useState("");
+  const [isPublic, setIsPublic] = useState(false);
+  const [group, setGroup] = useState(null);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    if (groupDetails) {
+      setDescription(groupDetails.description);
+      setIsPublic(groupDetails.isPublic);
+      setGroup(groupDetails);
+      setName(groupDetails.name);
+    }
+  }, [groupDetails]);
+
   const handleKickMember = async (memberId) => {
     try {
       const response = await fetch(`/api/groups/${group._id}/${memberId}`, {
@@ -91,8 +101,7 @@ const GroupDetails = ({
         setGroups((prevGroups) =>
           prevGroups.filter((g) => g._id !== group._id)
         );
-        setIsRoomSelected(false);
-        setRoomID(null);
+        setSelectedGroup(null);
         setToggleChatView(true);
         router.replace("/chats");
         window.location.reload();
@@ -228,217 +237,226 @@ const GroupDetails = ({
     }
   };
 
-  const currentUser = group.currentUser;
+  const currentUser = group?.currentUser;
 
-  const isCurrentUserCreator = group.creator === currentUser;
-  const isCurrentUserModerator = group.moderators.includes(currentUser);
+  const isCurrentUserCreator = group?.creator === currentUser;
+  const isCurrentUserModerator = group?.moderators?.includes(currentUser);
 
   return (
-    <div className="flex flex-col justify-between bg-gray-100  dark:bg-gray-800 pb-2 relative">
-      <div className="flex justify-between p-3 pl-[5px] items-center gap-1 ">
-        <IoArrowBackCircleOutline
-          className="w-6 h-6 cursor-pointer"
-          onClick={() => setShowGroupDetails(false)}
-        />
-        <div className="flex justify-center gap-4 ml-[-10px] w-[95%] items-center ">
-          <span className="font-semibold text-base md:text-xl ">
-            {group.name}
-          </span>
-          {(isCurrentUserCreator || isCurrentUserModerator) && (
-            <Dialog>
-              <DialogTrigger asChild>
-                <MdEdit className="cursor-pointer" />
-              </DialogTrigger>
-              <DialogContent className="w-[67%]">
-                <DialogHeader>
-                  <DialogTitle>Edit Group Name</DialogTitle>
-                  <DialogDescription>
-                    Update the group name below:
-                  </DialogDescription>
-                </DialogHeader>
-                <Input
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  className="mt-4"
-                  maxLength={30}
-                />
-                <DialogFooter>
-                  <Button onClick={handleUpdateName}>Save</Button>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-          )}
-        </div>
-        {!isCurrentUserCreator && (
-          <AlertDialog>
-            <AlertDialogTrigger>
-              <IoMdExit className="w-[22px] h-[22px] right-[7px] cursor-pointer top-5 absolute font-extralight" />
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>
-                  Are you sure you want to leave the group?
-                </AlertDialogTitle>
-                <AlertDialogDescription>
-                  Once you leave the group, you will no longer have access to
-                  its discussions and content.
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <AlertDialogAction
-                  className="bg-red-500"
-                  onClick={handleExitGroup}
-                >
-                  Continue
-                </AlertDialogAction>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
-        )}
-      </div>
-
-      <div className="p-4 mx-2 border-2 h-[32rem] rounded-sm overflow-y-auto bg-white">
-        {isCurrentUserCreator && (
-          <div className="flex items-center justify-end">
-            <Label htmlFor="privacy-switch" className="mr-2 text-sm">
-              {isPublic ? "Public" : "Private"}
-            </Label>
-            <Switch
-              id="privacy-switch"
-              checked={!isPublic}
-              onCheckedChange={togglePrivacy}
-            />
-          </div>
-        )}
-        <div className="my-4">
-          <div className="flex justify-between items-center">
-            <p className="font-medium mb-2">Description :</p>{" "}
+    <>
+      <div className="flex flex-col justify-between bg-gray-100 dark:bg-gray-800 pb-2 relative">
+        <div className="flex justify-between p-6 items-center gap-1 ">
+          <IoArrowBackCircleOutline
+            className="cursor-pointer"
+            size={30}
+            onClick={() => setShowGroupDetails(false)}
+          />
+          <div className="flex justify-center gap-4 ml-[-10px] w-[95%] items-center ">
+            <span className="font-semibold text-base md:text-xl ">
+              {group?.name}
+            </span>
             {(isCurrentUserCreator || isCurrentUserModerator) && (
               <Dialog>
                 <DialogTrigger asChild>
                   <MdEdit className="cursor-pointer" />
                 </DialogTrigger>
-                <DialogContent className="w-[70%]">
+                <DialogContent className="w-[67%]">
                   <DialogHeader>
-                    <DialogTitle>Edit Group Description</DialogTitle>
+                    <DialogTitle>Edit Group Name</DialogTitle>
                     <DialogDescription>
-                      Update the group description below:
+                      Update the group name below:
                     </DialogDescription>
                   </DialogHeader>
                   <Input
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     className="mt-4"
-                    maxLength={70}
+                    maxLength={30}
                   />
                   <DialogFooter>
-                    <Button onClick={handleUpdateDescription}>Save</Button>
+                    <Button onClick={handleUpdateName}>Save</Button>
                   </DialogFooter>
                 </DialogContent>
               </Dialog>
             )}
           </div>
-
-          <p className="text-sm shadow-md px-2 font-sans py-3 rounded-lg bg-gray-100">
-            {group.description}
-          </p>
+          {!isCurrentUserCreator && (
+            <AlertDialog>
+              <AlertDialogTrigger>
+                <IoMdExit
+                  size={24}
+                  className="cursor-pointer font-extralight"
+                />
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>
+                    Are you sure you want to leave the group?
+                  </AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Once you leave the group, you will no longer have access to
+                    its discussions and content.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction
+                    className="bg-red-500"
+                    onClick={handleExitGroup}
+                  >
+                    Continue
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          )}
         </div>
-        <div className="my-3 flex justify-between items-center">
-          <div className="text-sm text-gray-500">
-            Created on{" "}
-            {new Date(group.createdAt).toLocaleDateString("en-US", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })}
+
+        <div className="p-4 mx-2 border-2 h-[34rem] rounded-sm overflow-y-auto bg-white">
+          {isCurrentUserCreator && (
+            <div className="flex items-center justify-end">
+              <Label htmlFor="privacy-switch" className="mr-2 text-sm">
+                {isPublic ? "Public" : "Private"}
+              </Label>
+              <Switch
+                id="privacy-switch"
+                checked={!isPublic}
+                onCheckedChange={togglePrivacy}
+              />
+            </div>
+          )}
+          <div className="my-4">
+            <div className="flex justify-between items-center">
+              <p className="font-medium mb-2">Description :</p>{" "}
+              {(isCurrentUserCreator || isCurrentUserModerator) && (
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <MdEdit className="cursor-pointer" />
+                  </DialogTrigger>
+                  <DialogContent className="w-[70%]">
+                    <DialogHeader>
+                      <DialogTitle>Edit Group Description</DialogTitle>
+                      <DialogDescription>
+                        Update the group description below:
+                      </DialogDescription>
+                    </DialogHeader>
+                    <Input
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                      className="mt-4"
+                      maxLength={70}
+                    />
+                    <DialogFooter>
+                      <Button onClick={handleUpdateDescription}>Save</Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              )}
+            </div>
+
+            <p className="text-sm shadow-md px-2 font-sans py-3 rounded-lg bg-gray-100">
+              {group?.description}
+            </p>
           </div>
-        </div>
-        <div className="text-sm font-semibold">
-          {group.participants.length === 1
-            ? "1 Member"
-            : `${group.participants.length} Members`}
-        </div>
+          <div className="my-3 flex justify-between items-center">
+            <div className="text-sm text-gray-500">
+              Created on{" "}
+              {new Date(group?.createdAt).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </div>
+          </div>
+          <div className="text-sm font-semibold">
+            {group?.participants?.length === 1
+              ? "1 Member"
+              : `${group?.participants?.length} Members`}
+          </div>
 
-        <div className="my-3">
-          <ul className="list-none mt-2 space-y-2">
-            {group.participants.map((participant) => (
-              <li
-                key={participant._id}
-                className="flex items-center justify-between text-sm"
-              >
-                <div className="flex items-center gap-2">
-                  <Image
-                    alt="Participant Avatar"
-                    src={participant.profilePic}
-                    className="rounded-full"
-                    width={32}
-                    height={32}
-                  />
-                  <span>
-                    {participant.name}{" "}
-                    {group.creator === participant._id && (
-                      <span className="text-xs text-gray-500 ml-3">
-                        Creator
-                      </span>
-                    )}
-                    {group.creator !== participant._id &&
-                      group.moderators.includes(participant._id) && (
+          <div className="my-3">
+            <ul className="list-none mt-2 space-y-2">
+              {group?.participants?.map((participant) => (
+                <li
+                  key={participant._id}
+                  className="flex items-center justify-between text-sm"
+                >
+                  <div className="flex items-center gap-2">
+                    <Image
+                      alt="Participant Avatar"
+                      src={participant.profilePic}
+                      className="rounded-full"
+                      width={32}
+                      height={32}
+                    />
+                    <span>
+                      {participant.name}{" "}
+                      {group?.creator === participant._id && (
                         <span className="text-xs text-gray-500 ml-3">
-                          Moderator
+                          Creator
                         </span>
                       )}
-                  </span>
-                </div>
-                {(isCurrentUserCreator || isCurrentUserModerator) &&
-                  participant._id !== currentUser &&
-                  group.creator !== participant._id && (
-                    <DropdownMenu>
-                      <DropdownMenuTrigger>
-                        <Button variant="ghost" size="sm">
-                          •••
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent>
-                        {isCurrentUserCreator && (
-                          <>
-                            {group.moderators.includes(participant._id) ? (
-                              <DropdownMenuItem
-                                onSelect={() =>
-                                  handleModeratorStatus(
-                                    participant._id,
-                                    "remove"
-                                  )
-                                }
-                              >
-                                Remove Moderator
-                              </DropdownMenuItem>
-                            ) : (
-                              <DropdownMenuItem
-                                onSelect={() =>
-                                  handleModeratorStatus(participant._id, "make")
-                                }
-                              >
-                                Make Moderator
-                              </DropdownMenuItem>
-                            )}
-                          </>
+                      {group?.creator !== participant._id &&
+                        group?.moderators?.includes(participant._id) && (
+                          <span className="text-xs text-gray-500 ml-3">
+                            Moderator
+                          </span>
                         )}
+                    </span>
+                  </div>
+                  {(isCurrentUserCreator || isCurrentUserModerator) &&
+                    participant._id !== currentUser &&
+                    group?.creator !== participant._id && (
+                      <DropdownMenu>
+                        <DropdownMenuTrigger>
+                          <Button variant="ghost" size="sm">
+                            •••
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                          {isCurrentUserCreator && (
+                            <>
+                              {group?.moderators?.includes(participant._id) ? (
+                                <DropdownMenuItem
+                                  onSelect={() =>
+                                    handleModeratorStatus(
+                                      participant._id,
+                                      "remove"
+                                    )
+                                  }
+                                >
+                                  Remove Moderator
+                                </DropdownMenuItem>
+                              ) : (
+                                <DropdownMenuItem
+                                  onSelect={() =>
+                                    handleModeratorStatus(
+                                      participant._id,
+                                      "make"
+                                    )
+                                  }
+                                >
+                                  Make Moderator
+                                </DropdownMenuItem>
+                              )}
+                            </>
+                          )}
 
-                        <DropdownMenuItem
-                          onSelect={() => handleKickMember(participant._id)}
-                        >
-                          Remove Member
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  )}
-              </li>
-            ))}
-          </ul>
+                          <DropdownMenuItem
+                            onSelect={() => handleKickMember(participant._id)}
+                          >
+                            Remove Member
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    )}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
